19929929b0e92b4f3c41a96a1f5eacf092b47590
Ayo <ayoraheem0000@gmail.com>
2025-06-24 13:39:35 -0500

    Update frauddetection-app.py

--- frauddetection-app.py
+++ frauddetection-app.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyNi8o+NCyeRWiqEDQFBfZjE",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
